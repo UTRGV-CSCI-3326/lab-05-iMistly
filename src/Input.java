e3ff68f21e300826efdc5ebd692b4aa19485b221
MistlyH <craftyminer22@gmail.com>
2025-01-27 19:47:14 -0600

    Give me your information >:(

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Input{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter your weight(lb): ");
+        double weight = scanner.nextDouble();
+        System.out.print("Do you have a habit of smoking?(true/false): ");
+        boolean isSmoker = scanner.nextBoolean();
+        scanner.close();
+
+        System.out.println("\nHello, " + name + "!");
+        System.out.println("You are " + age + " year(s) old.");
+        System.out.println("You currently weigh " + weight + "lbs.");
+        if (isSmoker){
+            System.out.println("You are a smoker...");
+        }
+        else{
+            System.out.println("You are not a smoker!");
+        }
+    }
+}
